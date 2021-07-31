dd6da5b4742cddf81698d81ae4b1373479dad5d6
Mohammed Abdelaal <Mahmed.tareq.ali@gmail.com>
2021-07-31 17:06:01 +0200

    Theme adjustments

--- app/src/main/java/com/example/coffea/MainActivity.java
+++ app/src/main/java/com/example/coffea/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.coffea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
+
+
+
+
+
 }   
