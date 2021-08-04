e3e299fff2957ee8581266de2ed9ccf3f73105fb
Mohammed Abdelaal <Mahmed.tareq.ali@gmail.com>
2021-08-04 11:27:23 +0200

    Main OnBoardingActivity designed.

--- app/src/main/java/com/example/coffea/OnBoardingActivity.java
+++ app/src/main/java/com/example/coffea/OnBoardingActivity.java
@@ -2,7 +2,9 @@ package com.example.coffea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class OnBoardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+    }
+
+
+    public void toHome (View view)
+    {
+        Intent goToHome = new Intent (OnBoardingActivity.this, HomeActivity.class);
+        startActivity(goToHome);
     }
 }
